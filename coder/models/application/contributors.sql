a3e1687b71c911a17729c82fbee7dfcdf04a7a27
Matteo00000 <matteohenry00@gmail.com>
2025-01-24 16:50:33 +0100

    .

--- coder/models/application/contributors.sql
+++ coder/models/application/contributors.sql
@@ -1,5 +1,5 @@
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['ta_type']) }} AS ta_type_id,
-    ta_type AS ta_type_name
+    {{ dbt_utils.generate_surrogate_key(['contributors']) }} AS id,
+    contributors AS ta_type_name
 FROM {{ ref('st_park_p') }}
-GROUP BY ta_type
+GROUP BY contributors
