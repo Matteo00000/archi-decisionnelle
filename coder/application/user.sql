0b0f41ff1dabd251228c8ab876a6c9ff7c849b74
Qawa <95877116+Qawa-ls@users.noreply.github.com>
2025-01-25 16:38:45 +0100

    Delete coder/application directory

--- coder/application/user.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-    id AS user_id,
-    avatar_url AS avatar
-FROM {{ ref('contributors') }} 
-    JOIN {{ ref('user_view_type') }} ON user_view_type = user_view_type_name
--- coder/application/user_view_type.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    {{ dbt_utils.generate_surrogate_key(['user_view_type']) }} AS user_view_type_id,
-    user_view_type AS user_view_type_name
-FROM {{ ref('contributors') }}
-GROUP BY user_view_type
